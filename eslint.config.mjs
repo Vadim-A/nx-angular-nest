672310263f1c762e1fd561294e8a1608430c73ba
Vadim <golovachyov@gmail.com>
2025-09-01 02:21:46 +0400

    feat: add linter rules and other improvements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,5 @@
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginImportX from 'eslint-plugin-import-x';
 import nx from '@nx/eslint-plugin';
 
 export default [
@@ -36,7 +38,52 @@ export default [
       '**/*.cjs',
       '**/*.mjs',
     ],
-    // Override or add rules here
-    rules: {},
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+    },
   },
+
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      'import-x': eslintPluginImportX,
+    },
+    settings: {
+      'import-x/parsers': {
+        '@typescript-eslint/parser': ['.ts'],
+      },
+      'import-x/resolver': {
+        typescript: {
+          project: ['tsconfig.base.json'],
+          alwaysTryTypes: true,
+        },
+      },
+    },
+    rules: {
+      'import-x/no-cycle': 'error',
+      'import-x/order': [
+        'error',
+        {
+          'newlines-between': 'always',
+          named: true,
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+
+  {
+    files: ['**/*.html'],
+    rules: {
+      '@angular-eslint/template/attributes-order': ['error'],
+    },
+  },
+
+  eslintConfigPrettier,
 ];
