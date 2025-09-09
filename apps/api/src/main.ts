997da3ef2c0ed22f4e0154a584c6096b1d9fcc7f
Vadim-A <Vadim-A@users.noreply.github.com>
2025-09-10 02:34:48 +0400

    feat: update prettier, configurate linter and prettier, run prettier

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap(): Promise<void> {
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
   );
 }
 
